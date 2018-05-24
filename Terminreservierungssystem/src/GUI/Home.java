e70d1484386ab3208aa3adb6fa51a797c90cde0d
mskanda-tgm <mskanda@student.tgm.ac.at>
2018-05-24 13:18:57 +0200

    Register und Login GUI für Reservierungssystem

---
+++ Terminreservierungssystem/src/GUI/Home.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Home {
+    private JPanel panel1;
+    private JButton logoutButton;
+    private JButton createEventButton;
+    private JButton myEventsButton;
+}
