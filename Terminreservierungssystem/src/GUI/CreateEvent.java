f7e3ae3ddc18c7a2aa9a111f279c7e6b85edd743
mskanda-tgm <mskanda@student.tgm.ac.at>
2018-06-01 11:52:56 +0200

    Design-Änderung

--- Terminreservierungssystem/src/GUI/CreateEvent.java
+++ Terminreservierungssystem/src/GUI/CreateEvent.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 
 public class CreateEvent {
     private JTextField textField1;
-    private JTextField textField2;
     private JTextArea textArea1;
-    private JComboBox comboBox1;
     private JButton createButton;
 }
